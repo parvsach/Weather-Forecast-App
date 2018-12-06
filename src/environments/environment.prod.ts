7a4fe953062048f8b4ff160256cf3e225328fd09
Parveen Sachdeva <sachdeva176@gmail.com>
2018-12-06 22:09:39 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   appId: '13701bbe30541938390ed1b0b5e749fd',
-  baseUrl: 'http://api.openweathermap.org/data/2.5/', 
+  baseUrl: 'https://api.openweathermap.org/data/2.5/', 
   units:'imperial'
 };
